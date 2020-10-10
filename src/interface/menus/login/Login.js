243faca9457082b5e17982876c86c558718018a7
wizguin <16276645+wizguin@users.noreply.github.com>
2020-10-10 20:23:42 +0100

    Modularised components

--- src/interface/menus/login/Login.js
+++ src/interface/menus/login/Login.js
@@ -1,4 +1,4 @@
-import Button from '../../../components/Button'
+import { Button } from '../../../components/components'
 
 
 /* START OF COMPILED CODE */
--- src/interface/menus/start/Start.js
+++ src/interface/menus/start/Start.js
@@ -1,63 +1,63 @@
-import Button from '../../../components/Button'
+import { Button } from '../../../components/components'
 
 
 /* START OF COMPILED CODE */
 
 class Start extends Phaser.Scene {
-    
+
     constructor() {
         super("Start");
-        
+
         /* START-USER-CTR-CODE */
         // Write your code here.
         /* END-USER-CTR-CODE */
     }
-    
+
     create() {
-        
+
         // bg
         const bg = this.add.image(0, 0, "load", "bg");
         bg.setOrigin(0, 0);
-        
+
         // christmas
         this.add.image(760, 420, "start", "christmas");
-        
+
         // bottom
         this.add.image(760, 766, "start", "bottom");
-        
+
         // blog-text
         this.add.image(364, 884, "start", "blog-text");
-        
+
         // blog
         this.add.image(364, 812, "start", "blog");
-        
+
         // new
         this.add.image(1141, 775, "start", "new");
-        
+
         // member
         this.add.image(1157, 860, "start", "member");
-        
+
         // penguin_1
         this.add.image(1268, 771, "start", "penguin-1");
-        
+
         // penguin_2
         this.add.image(1264, 855, "start", "penguin-2");
-        
+
         // start_button
         const start_button = this.add.sprite(760, 826, "start", "start-button");
-        
+
         // start_text
         this.add.image(760, 826, "start", "start-text");
-        
+
         // logo
         this.add.image(760, 682, "start", "logo");
-        
+
         // start_button (components)
         const start_buttonButton = new Button(start_button);
         start_buttonButton.spriteName = "start-button";
         start_buttonButton.callback = () => this.onStartClick();
     }
-    
+
     /* START-USER-CODE */
 
     onStartClick() {
