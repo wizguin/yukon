be0e715ed08404b6cbaf348ada660ad0af44faae
wizguin <16276645+wizguin@users.noreply.github.com>
2020-10-22 19:56:25 +0100

    Cleaned up preloading

--- src/preload/Preload.js
+++ src/preload/Preload.js
@@ -1,36 +1,22 @@
 import Load from '@/interface/menus/load/Load'
 import Start from '@/interface/menus/start/Start'
 import Login from '@/interface/menus/login/Login'
+import World from '@/world/World'
 
 
 export default class Preload extends Phaser.Scene {
 
     preload() {
-        this.baseURL = 'assets/media/interface/menus'
+        this.load.pack('preload', 'assets/media/preload/preload-pack.json')
 
-        this.preloadScenes({
-            'load': { sceneKey: 'Load', scene: Load },
-            'start': { sceneKey: 'Start', scene: Start },
-            'login': { sceneKey: 'Login', scene: Login }
-        })
-    }
-
-    preloadScenes(scenes) {
-        for (let key in scenes) {
-            let scene = scenes[key]
-
-            this.load.multiatlas({
-                key: key,
-                atlasURL: `${this.baseURL}/${key}/${key}.json`,
-                path: `${this.baseURL}/${key}`
-            })
-
-            this.scene.add(scene.sceneKey, scene.scene)
-        }
+        this.scene.add('Load', Load)
+        this.scene.add('Start', Start)
+        this.scene.add('Login', Login)
+        this.scene.add('World', World)
     }
 
     create() {
-        this.scene.start('Start')
+        this.scene.start('Login')
     }
 
 }
