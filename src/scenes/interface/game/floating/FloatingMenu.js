9ca2e34913ce302d85c41b2058a4bed6b774fd71
wizguin <16276645+wizguin@users.noreply.github.com>
2020-11-29 21:44:25 +0000

    Added FloatingMenu

---
+++ src/scenes/interface/game/floating/FloatingMenu.js
@@ -0,0 +1,41 @@
+import BaseContainer from '@scenes/base/BaseContainer'
+
+
+export default class FloatingMenu extends BaseContainer {
+
+    initMenu() {
+        if (this.safe) this.setupSafe()
+        if (this.close) this.setupClose()
+
+        this.createBack()
+    }
+
+    setupSafe() {
+        this.safe.isFilled = false
+        this.safe.setInteractive()
+    }
+
+    setupClose() {
+        this.close.isFilled = false
+        this.close.setInteractive({ cursor: 'pointer' })
+
+        this.close.on('pointerup', () => {
+            this.visible = false
+        })
+    }
+
+    createBack() {
+        let back = this.scene.add.rectangle(-this.x, -this.y, 1520, 960)
+
+        back.setOrigin(0)
+        back.setInteractive()
+
+        this.add(back)
+        this.sendToBack(back)
+
+        back.on('pointerover', () => {
+            this.visible = false
+        })
+    }
+
+}
