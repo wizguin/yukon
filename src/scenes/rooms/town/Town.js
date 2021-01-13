b2617b14375f9916ec300323a0581c9865dec081
wizguin <16276645+wizguin@users.noreply.github.com>
2021-01-13 15:42:58 +0000

    Updated town

--- src/scenes/rooms/town/Town.js
+++ src/scenes/rooms/town/Town.js
@@ -42,41 +42,13 @@ class Town extends RoomScene {
         const coffee_door = this.add.image(449, 305, "town", "coffee_door");
         coffee_door.setOrigin(0, 0);
 
-        // chair_2
-        const chair_2 = this.add.image(438, 462, "town", "chair_2");
-        chair_2.setOrigin(0.5, 0.27358490566037735);
-
-        // table_2
-        const table_2 = this.add.image(483, 522, "town", "table_2");
-        table_2.setOrigin(0.5, 0.79126214);
-
-        // chair_1
-        const chair_1 = this.add.image(401, 511, "town", "chair_1");
-        chair_1.setOrigin(0.5, 0.2857142857142857);
-
-        // table_1
-        const table_1 = this.add.image(454, 575, "town", "table_1");
-        table_1.setOrigin(0.5, 0.7830188679245284);
-
-        // box_1
-        const box_1 = this.add.image(973, 432, "town", "box_1");
-        box_1.setOrigin(0.5, 0.63432836);
-
-        // box_2
-        const box_2 = this.add.image(955, 483, "town", "box_2");
-        box_2.setOrigin(0.5, 0.67894737);
-
-        // box_3
-        const box_3 = this.add.image(1148, 582, "town", "box_3");
-        box_3.setOrigin(0.5, 0.56329114);
-
         // gift_door
         const gift_door = this.add.image(995, 294, "town", "gift_door");
         gift_door.setOrigin(0, 0);
 
         // canopy
-        const canopy = this.add.image(693, 464, "town", "canopy");
-        canopy.setOrigin(0.5, 0.94036697);
+        const canopy = this.add.image(692, 464, "town", "canopy");
+        canopy.setOrigin(0.49606299212598426, 0.9403669724770642);
 
         // canopy_stars
         const canopy_stars = this.add.image(647, 348, "town", "canopy_stars");
@@ -90,8 +62,36 @@ class Town extends RoomScene {
         const lights0001 = this.add.image(518, -75, "town", "lights0001");
         lights0001.setOrigin(0, 0);
 
+        // box_1
+        const box_1 = this.add.image(973, 432, "town", "box_1");
+        box_1.setOrigin(0.49382716049382713, 0.6268656716417911);
+
+        // box_2
+        const box_2 = this.add.image(954, 483, "town", "box_2");
+        box_2.setOrigin(0.49504950495049505, 0.6736842105263158);
+
+        // box_3
+        const box_3 = this.add.image(1148, 581, "town", "box_3");
+        box_3.setOrigin(0.4943820224719101, 0.5569620253164557);
+
+        // chair_2
+        const chair_2 = this.add.image(436, 462, "town", "chair_2");
+        chair_2.setOrigin(0.49382716049382713, 0.27358490566037735);
+
+        // table_2
+        const table_2 = this.add.image(485, 523, "town", "table_2");
+        table_2.setOrigin(0.5, 0.7864077669902912);
+
+        // chair_1
+        const chair_1 = this.add.image(402, 511, "town", "chair_1");
+        chair_1.setOrigin(0.49411764705882355, 0.2857142857142857);
+
+        // table_1
+        const table_1 = this.add.image(455, 575, "town", "table_1");
+        table_1.setOrigin(0.49572649572649574, 0.7830188679245284);
+
         // lists
-        const sort = [fg, chair_1, table_1, chair_2, table_2, box_1, box_2, box_3]
+        const sort = [fg]
 
         this.sort = sort;
     }
