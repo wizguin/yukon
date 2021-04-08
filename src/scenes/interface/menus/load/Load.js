718a65f4d46aa4f621cf7a549d8a0ecdd09e2ad1
wizguin <16276645+wizguin@users.noreply.github.com>
2021-04-08 12:36:46 +0100

    Added fixed size to load text

--- src/scenes/interface/menus/load/Load.js
+++ src/scenes/interface/menus/load/Load.js
@@ -37,7 +37,7 @@ class Load extends BaseScene {
         // text
         const text = this.add.text(760, 520, "", {});
         text.setOrigin(0.5, 0);
-        text.setStyle({"fontFamily":"Arial","fontSize":"32px"});
+        text.setStyle({"align":"center","fixedWidth":800,"fixedHeight":40,"fontFamily":"Arial","fontSize":"32px"});
 
         // bar
         const bar = this.add.container(760, 480);
