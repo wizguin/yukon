c99f171f65f08eb2db6810a05f78949d72cdaf5b
wizguin <16276645+wizguin@users.noreply.github.com>
2021-12-30 18:48:33 +0000

    Added version text

--- src/Game.js
+++ src/Game.js
@@ -9,6 +9,8 @@ export default class Game extends Phaser.Game {
     constructor(config) {
         super(config)
 
+        console.log(`%cYukon ${VERSION}`, 'color: white; background: black; border-left: 8px solid #0280cd; padding: 0px 8px 0px 8px;')
+
         this.crumbs = config.crumbs
         this.network = new Network(this)
 
