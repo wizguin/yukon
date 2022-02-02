c3370107b7dbc8dae37708e1d4f7a2d3fe10806c
floogal <67554336+floogal@users.noreply.github.com>
2022-02-02 19:32:53 +0000

    fix indentation

--- src/scenes/rooms/RoomScene.js
+++ src/scenes/rooms/RoomScene.js
@@ -45,13 +45,13 @@ export default class RoomScene extends BaseScene {
         this.interface.showInterface()
     }
 	
-	preload() {
-		if (this.music && !this.cache.audio.exists(this.music)) {
+    preload() {
+        if (this.music && !this.cache.audio.exists(this.music)) {
             this.load.audio(this.music, `assets/media/music/${this.music}.mp3`)
         }
 		
-		this._preload()
-	}
+        this._preload()
+    }
 
     sortChildren() {
         if (!this.sort) return
