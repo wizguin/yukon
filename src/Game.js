de11369dbe3f86885a8b8de80de4e580fa71af1e
wizguin <16276645+wizguin@users.noreply.github.com>
2022-04-28 13:36:54 +0100

    Updated Game.js

--- src/Game.js
+++ src/Game.js
@@ -10,7 +10,11 @@ export default class Game extends Phaser.Game {
     constructor(config) {
         super(config)
 
-        console.log(`Yukon ${VERSION} https://github.com/wizguin/yukon`)
+        // Please leave this line here for credit purposes
+        console.log('%cYukon%c https://github.com/wizguin/yukon', 'color: white; background: black; border-left: 8px solid #0099cc; padding: 0px 8px 0px 8px;', '')
+
+        // "Yukon" can be changed here to display the version of your own client
+        console.log(`Yukon ${VERSION}`)
 
         this.initContainers()
 
