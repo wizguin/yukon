8dbba1aac21d7bb81e6bbde39a7914316f814514
wizguin <16276645+wizguin@users.noreply.github.com>
2023-04-11 22:22:50 +0100

    cardjitsu: Add SenseiMenu

--- src/scenes/games/sensei/Sensei.js
+++ src/scenes/games/sensei/Sensei.js
@@ -1,10 +1,8 @@
-
-// You can write more code here
-
 /* START OF COMPILED CODE */
 
 import GameScene from "../GameScene";
 import SenseiWidget from "./widget/SenseiWidget";
+import SenseiMenu from "./menu/SenseiMenu";
 /* START-USER-IMPORTS */
 /* END-USER-IMPORTS */
 
@@ -15,6 +13,8 @@ export default class Sensei extends GameScene {
 
         /** @type {SenseiWidget} */
         this.sensei;
+        /** @type {SenseiMenu} */
+        this.menu;
 
 
         /* START-USER-CTR-CODE */
@@ -35,7 +35,12 @@ export default class Sensei extends GameScene {
         this.add.existing(sensei);
         sensei.visible = true;
 
+        // menu
+        const menu = new SenseiMenu(this, 1055, 774);
+        this.add.existing(menu);
+
         this.sensei = sensei;
+        this.menu = menu;
 
         this.events.emit("scene-awake");
     }
@@ -45,6 +50,8 @@ export default class Sensei extends GameScene {
 
     create() {
         super.create()
+
+        this.sensei.showSpeech('Hello world')
     }
 
     /* END-USER-CODE */
---
+++ src/scenes/games/sensei/menu/SenseiMenu.js
@@ -0,0 +1,32 @@
+/* START OF COMPILED CODE */
+
+import BaseContainer from "../../../base/BaseContainer";
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class SenseiMenu extends BaseContainer {
+
+    constructor(scene, x, y) {
+        super(scene, x ?? 0, y ?? 0);
+
+        /** @type {Phaser.GameObjects.Image} */
+        this.bg;
+
+
+        // bg
+        const bg = scene.add.image(0, 0, "sensei", "menu/bg");
+        bg.setOrigin(0.5, 0.5015197568389058);
+        this.add(bg);
+
+        this.bg = bg;
+
+        /* START-USER-CTR-CODE */
+        /* END-USER-CTR-CODE */
+    }
+
+
+    /* START-USER-CODE */
+    /* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
