5557a0552e2e29c5846674131998194d74a22ad7
wizguin <16276645+wizguin@users.noreply.github.com>
2023-07-19 22:22:02 +0100

    Fixed issue with widgets closing in showWidget

--- src/engine/interface/InterfaceController.js
+++ src/engine/interface/InterfaceController.js
@@ -138,10 +138,6 @@ export default class InterfaceController extends BaseScene {
     }
 
     showWidget(widget) {
-        if (widget.visible) {
-            widget.close()
-        }
-
         if (widget.widgetLayer) {
             widget.widgetLayer.bringToTop(widget)
         }
