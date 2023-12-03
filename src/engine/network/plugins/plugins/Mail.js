97bae8ff8697029d8cd663bb2e5a07f20445182d
wizguin <16276645+wizguin@users.noreply.github.com>
2023-12-03 15:08:36 +0000

    mail: Handle send mail

--- src/engine/network/plugins/plugins/Mail.js
+++ src/engine/network/plugins/plugins/Mail.js
@@ -7,7 +7,32 @@ export default class Mail extends Plugin {
         super(network)
 
         this.events = {
+            'send_mail': this.sendMail,
             'receive_mail': this.receiveMail
+        }
+
+        this.responses = {
+            InsufficientCoins: 0,
+            FullInbox: 1,
+            Success: 2
+        }
+    }
+
+    sendMail(args) {
+        this.world.client.coins = args.coins
+
+        switch (args.response) {
+            case this.responses.InsufficientCoins:
+                this.showInsufficientCoins()
+                break
+
+            case this.responses.FullInbox:
+                this.showFullInbox()
+                break
+
+            case this.responses.Success:
+                this.showSuccess()
+                break
         }
     }
 
@@ -15,6 +40,22 @@ export default class Mail extends Plugin {
         this.world.client.postcards.push(args)
 
         this.world.client.refreshPostcards()
+    }
+
+    showInsufficientCoins() {
+        this.showError('Failed to send Postcard.\nYou need more coins.')
+    }
+
+    showFullInbox() {
+        this.showError('This penguins mailbox is full. Your postcard could not be sent. Please try again later.')
+    }
+
+    showSuccess() {
+        this.interface.prompt.showMailSuccess('Postcard sent')
+    }
+
+    showError(text) {
+        this.interface.prompt.showMailError(text)
     }
 
 }
